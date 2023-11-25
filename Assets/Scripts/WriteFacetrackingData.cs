93fa5c2261e91a161489e37598aff1b61248f39a
Tobias Weiss <tobias@tobias-weiss.org>
2023-11-25 15:08:57 +0100

    w

--- Assets/Scripts/WriteFacetrackingData.cs
+++ Assets/Scripts/WriteFacetrackingData.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Gradle;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WriteFacetrackingData : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        FileWriter w = gameObject.AddComponent<FileWriter>();
+        Button b = GetComponent<Button>();
+        FTTest f = GameObject.Find("FTManager").GetComponent<FTTest>();
+        b.onClick.AddListener(Submit);
     }
 
-    // Update is called once per frame
-    void Update()
+    void Submit()
     {
-        
+
     }
 }
